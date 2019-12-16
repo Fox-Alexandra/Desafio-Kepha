e237973bb1b57bab564edd042ac3a50d50880b04
Alexandra da Silveira <Fox-4lexandra@users.noreply.github.com>
2019-12-16 11:44:09 -0300

    Exercício 2 em andamento

--- TesteDeLogica/Exercício2/Program.cs
+++ TesteDeLogica/Exercício2/Program.cs
@@ -6,13 +6,19 @@ namespace Exercício2
     {
         static void Main(string[] args)
         {
-            //Encontre o menor valor positivo inteiro que não pode ser representado como uma soma de um subconjunto em um array.
-            // Dado um array ordenado (ordem crescente) de números positivos, encontre o menor valor positivo inteiro que não pode ser representado como a soma de elementos de um subconjunto deste array.
-            // Complexidade experada é 0(n).
-            // Exemplos:
-            // Input:  arr[] = {1, 3, 6, 10, 11, 15};
-            // Output: 2
-            Console.WriteLine("Hello World!");
+            int[] valor = new int [3];
+
+            int soma = 0;
+            foreach (var item in valor)
+            {
+                soma += item;
+                if (valor == soma)
+                {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
         }
     }
 }
